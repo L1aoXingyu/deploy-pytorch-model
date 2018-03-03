dae6bc139862501810694e377a99c4a8d7152756
xyliao <xyliao1993@qq.com>
2018-03-03 19:28:41 +0800

    Initial commit

---
+++ run_pytorch_server.py
@@ -0,0 +1,101 @@
+# encoding: utf-8
+"""
+@author: xyliao
+@contact: xyliao1993@qq.com
+"""
+
+import io
+import json
+
+import flask
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms as tfs
+from torchvision.models import resnet50
+
+# Initialize our Flask application and the PyTorch model.
+app = flask.Flask(__name__)
+model = None
+use_gpu = False
+
+with open('imagenet_class.txt', 'r') as f:
+    idx2label = eval(f.read())
+
+
+def load_model():
+    """Load the pre-trained model, you can use your model just as easily.
+
+    """
+    global model
+    model = resnet50(pretrained=True)
+    model.eval()
+    if use_gpu:
+        model.cuda()
+
+
+def prepare_image(image, target_size):
+    """Do image preprocessing before prediction on any data.
+
+    :param image: Original image
+    :param target_size: Target image size
+    :return:
+        preprocessed image
+    """
+
+    if image.mode != 'RGB':
+        image = image.convert("RGB")
+
+    # Resize the input image nad preprocess it.
+    image = tfs.Resize(target_size)(image)
+    image = tfs.ToTensor()(image)
+
+    # Convert to Torch.Tensor and normalize.
+    image = tfs.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(image)
+
+    # Add batch_size axis.
+    image = image[None]
+    if use_gpu:
+        image = image.cuda()
+    return torch.autograd.Variable(image)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Initialize the data dictionary that will be returned from the view.
+    data = {"success": False}
+
+    # Ensure an image was properly uploaded to our endpoint.
+    if flask.request.method == 'POST':
+        if flask.request.files.get("image"):
+            # Read the image in PIL format
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            # Preprocess the image nad prepare it for classification.
+            image = prepare_image(image, target_size=(224, 224))
+
+            # Classify the input image and then initialize the list of predictions to return to the client.
+            preds = F.softmax(model(image), dim=1)
+            results = torch.topk(preds.cpu().data, k=3, dim=1)
+
+            data['predictions'] = list()
+
+            # Loop over the results and add them to the list of returned predictions
+            for prob, label in zip(results[0][0], results[1][0]):
+                label_name = idx2label[label]
+                r = {"label": label_name, "probability": float(prob)}
+                data['predictions'].append(r)
+
+            # Indicate that the request was a success.
+            data["success"] = True
+
+    # Return the data dictionary as a JSON response.
+    return flask.jsonify(data)
+
+
+if __name__ == '__main__':
+    print("Loading PyTorch model and Flask starting server ...")
+    print("Please wait until server has fully started")
+    load_model()
+    app.run()
---
+++ simple_request.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+"""
+@author: xyliao
+@contact: xyliao1993@qq.com
+"""
+
+import requests
+
+# Initialize the PyTorch REST API endpoint URL.
+PyTorch_REST_API_URL = 'http://127.0.0.1:5000/predict'
+
+
+def predict_result(image_path):
+    # Initialize image path
+    image = open(image_path, 'rb').read()
+    payload = {'image': image}
+
+    # Submit the request.
+    r = requests.post(PyTorch_REST_API_URL, files=payload).json()
+
+    # Ensure the request was successful.
+    if r['success']:
+        # Loop over the predictions and display them.
+        for (i, result) in enumerate(r['predictions']):
+            print('{}. {}: {:.4f}'.format(i + 1, result['label'], result['probability']))
+
+    # Otherwise, the request failed.
+    else:
+        print('Request failed')
+
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire()
